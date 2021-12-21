f3f4d81b28c403811228d80b7071de15319feba9
httport <96192210+httport@users.noreply.github.com>
2021-12-21 19:21:38 +0400

    basic bot

--- src/main/kotlin/moe/kadosawa/ayami/App.kt
+++ src/main/kotlin/moe/kadosawa/ayami/App.kt
@@ -1,12 +1,41 @@
 package moe.kadosawa.ayami
 
-class App {
-    val greeting: String
-        get() {
-            return "Hello World!"
+import kotlinx.cli.ArgParser
+import kotlinx.cli.ArgType
+import kotlinx.cli.default
+import mu.KotlinLogging
+import net.dv8tion.jda.api.JDABuilder
+import net.dv8tion.jda.api.events.ReadyEvent
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent
+import net.dv8tion.jda.api.hooks.ListenerAdapter
+
+private val logger = KotlinLogging.logger {}
+
+class Bot : ListenerAdapter() {
+    override fun onSlashCommand(event: SlashCommandEvent) {
+        when (event.name) {
+            "ping" -> {
+                event.reply("Pong!").queue()
+
+            }
         }
+    }
+
+    override fun onReady(event: ReadyEvent) {
+        logger.info { "${event.jda.guilds}" }
+    }
 }
 
-fun main() {
-    println(App().greeting)
+fun main(args: Array<String>) {
+    val parser = ArgParser({}::class.java.packageName)
+
+    val configPath by parser.option(ArgType.String, fullName = "config", shortName = "c")
+        .default("config.properties")
+
+    parser.parse(args)
+    Config.readFromFile(configPath)
+
+    val jda = JDABuilder.createLight(Config.discordToken)
+        .addEventListeners(Bot())
+        .build()
 }
