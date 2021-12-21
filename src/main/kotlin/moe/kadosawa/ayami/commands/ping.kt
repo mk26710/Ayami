69b504f2eec474a7c0f86babd7b5667f4a270fda
httport <96192210+httport@users.noreply.github.com>
2021-12-21 20:55:07 +0400

    suppress is not needed here anymore

--- src/main/kotlin/moe/kadosawa/ayami/commands/ping.kt
+++ src/main/kotlin/moe/kadosawa/ayami/commands/ping.kt
@@ -2,7 +2,6 @@ package moe.kadosawa.ayami.commands
 
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent
 
-@Suppress("unused")
 fun handlePing(event: SlashCommandEvent) {
     event.reply("Pong!").queue()
 }
