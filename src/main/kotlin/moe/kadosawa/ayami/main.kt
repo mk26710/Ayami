2344a74adc9ef276c32a5a5e7b6386d61dc1af52
httport <96192210+httport@users.noreply.github.com>
2021-12-30 15:09:20 +0400

    tables init, some comments and added reminders slash command

--- src/main/kotlin/moe/kadosawa/ayami/main.kt
+++ src/main/kotlin/moe/kadosawa/ayami/main.kt
@@ -9,15 +9,23 @@ import kotlinx.coroutines.CompletableDeferred
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import moe.kadosawa.ayami.commands.PingSlash
+import moe.kadosawa.ayami.commands.ReminderAddSlash
 import moe.kadosawa.ayami.commands.ResinSlash
 import moe.kadosawa.ayami.interfaces.SlashExecutor
 import moe.kadosawa.ayami.listeners.MainListener
+import moe.kadosawa.ayami.tables.Reminders
 import moe.kadosawa.ayami.utils.Args
 import moe.kadosawa.ayami.utils.Config
+import moe.kadosawa.ayami.utils.dataSource
 import mu.KotlinLogging
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.requests.GatewayIntent
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.exists
+import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 import kotlin.properties.Delegates
+import kotlin.system.exitProcess
 
 private val logger = KotlinLogging.logger {}
 
@@ -50,11 +58,20 @@ fun main(args: Array<String>) = runBlocking<Unit> {
     Args.parser.parse(args)
     Config.fromFile(Args.configPath)
 
-    commands = mutableMapOf(
-        "ping" to PingSlash(),
-        "resin" to ResinSlash(),
-    )
+    Database.connect(dataSource)
 
+    if (Args.dbInit) {
+        newSuspendedTransaction {
+            if (!Reminders.exists()) {
+                SchemaUtils.create(Reminders)
+            }
+        }
+
+        logger.info { "Tables were created" }
+        exitProcess(0)
+    }
+
+    // Create jda instance
     jda = light(Config.discordToken) {
         intents += GatewayIntent.GUILD_MEMBERS
         intents += GatewayIntent.DIRECT_MESSAGES
@@ -63,5 +80,14 @@ fun main(args: Array<String>) = runBlocking<Unit> {
         addEventListeners(MainListener())
     }
 
+    // Create commands
+    commands = mutableMapOf(
+        "ping" to PingSlash(),
+        "resin" to ResinSlash(),
+        "reminder-add" to ReminderAddSlash()
+    )
+
+    // Suspended function awaiting
+    // completion of jdaIsReady
     launch { onceReady() }
 }
