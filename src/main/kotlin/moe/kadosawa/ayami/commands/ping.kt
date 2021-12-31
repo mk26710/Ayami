8422b98da19de6c6ee955f100706aae61aa63591
httport <96192210+httport@users.noreply.github.com>
2021-12-31 20:27:04 +0400

    better ping command

--- src/main/kotlin/moe/kadosawa/ayami/commands/ping.kt
+++ src/main/kotlin/moe/kadosawa/ayami/commands/ping.kt
@@ -1,17 +1,20 @@
 package moe.kadosawa.ayami.commands
 
-import kotlinx.coroutines.delay
 import moe.kadosawa.ayami.extensions.await
 import moe.kadosawa.ayami.extensions.isPrivate
 import moe.kadosawa.ayami.interfaces.SlashExecutor
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent
+import kotlin.system.measureTimeMillis
 
 class PingSlash : SlashExecutor() {
     override val path = "ping"
 
     override suspend fun execute(event: SlashCommandEvent) {
-        event.reply("Pong!").setEphemeral(event.isPrivate).await()
-        delay(3000)
-        event.hook.editOriginal("Ping-Pong!").await()
+        event.deferReply(event.isPrivate).await()
+        val rtt = measureTimeMillis {
+            event.hook.sendMessage("Pong...").await()
+        }
+
+        event.hook.editOriginal("Pong! (Took `$rtt`ms)").await()
     }
 }
