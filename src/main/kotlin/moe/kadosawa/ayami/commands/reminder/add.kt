4d8c9a28780fd1c8d6a07440372f93114eb517a5
httport <96192210+httport@users.noreply.github.com>
2022-01-02 20:46:02 +0400

    Updated reminder add slash command
     - Uses RemindersService
     - Now tells when the reminder will be triggered and what ID it has

--- src/main/kotlin/moe/kadosawa/ayami/commands/reminder/add.kt
+++ src/main/kotlin/moe/kadosawa/ayami/commands/reminder/add.kt
@@ -1,13 +1,12 @@
 package moe.kadosawa.ayami.commands.reminder
 
 import kotlinx.datetime.Clock
+import moe.kadosawa.ayami.database.services.RemindersService
+import moe.kadosawa.ayami.database.tables.NewReminder
 import moe.kadosawa.ayami.extensions.await
 import moe.kadosawa.ayami.extensions.isPrivate
 import moe.kadosawa.ayami.interfaces.SlashExecutor
-import moe.kadosawa.ayami.database.tables.Reminders
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent
-import org.jetbrains.exposed.sql.insert
-import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 import kotlin.time.Duration
 
 class ReminderAddSlash : SlashExecutor() {
@@ -19,26 +18,28 @@ class ReminderAddSlash : SlashExecutor() {
         // in order to make things a bit more accurate
         val now = Clock.System.now()
 
-        val durationToParse = event.getOption("duration")!!.asString
-        val userContent = event.getOption("content")!!.asString
+        val unparsedDuration = event.getOption("duration")!!.asString
+        val content = event.getOption("content")!!.asString
 
-        val duration = Duration.parseOrNull(durationToParse)
+        // Try to parse user's duration as Duration
+        val duration = Duration.parseOrNull(unparsedDuration)
         if (duration == null) {
             event.hook.sendMessage("You have provided an incorrect duration syntax!").await()
             return
         }
 
         val toTriggerAt = now + duration
-        val reminderId = newSuspendedTransaction {
-            Reminders.insert {
-                it[triggerAt] = toTriggerAt
-                it[authorId] = event.user.idLong
-                it[guildId] = event.guild?.idLong
-                it[channelId] = event.channel.idLong
-                it[content] = userContent
-            } get Reminders.id
-        }
 
-        event.hook.sendMessage("Reminder `#$reminderId` was created!").await()
+        val newReminder = NewReminder(
+            triggerAt = toTriggerAt,
+            authorId = event.user.idLong,
+            guildId = event.guild?.idLong,
+            channelId = event.channel.idLong,
+            content = content
+        )
+
+        val reminder = RemindersService.add(newReminder)
+        val formattedTime = "<t:${reminder.triggerAt.epochSeconds}:R>"
+        event.hook.sendMessage(":ok_hand: I will remind you about this $formattedTime (`#${reminder.id}`)").await()
     }
 }
