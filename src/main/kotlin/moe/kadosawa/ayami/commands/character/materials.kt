0c79c0b3dbe32ca1d9cbf83484f7898f5889753c
httport <96192210+httport@users.noreply.github.com>
2022-01-06 18:16:08 +0400

    Updated search method for characters command

--- src/main/kotlin/moe/kadosawa/ayami/commands/character/materials.kt
+++ src/main/kotlin/moe/kadosawa/ayami/commands/character/materials.kt
@@ -2,6 +2,7 @@ package moe.kadosawa.ayami.commands.character
 
 import moe.kadosawa.ayami.extensions.await
 import moe.kadosawa.ayami.extensions.isPrivate
+import moe.kadosawa.ayami.genshin.GenshinCharacters
 import moe.kadosawa.ayami.interfaces.SlashExecutor
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent
 import org.apache.commons.text.similarity.FuzzyScore
@@ -10,75 +11,33 @@ import java.util.*
 object CharacterMaterialsSlash : SlashExecutor() {
     override val path = "character/materials"
 
-    val characters = listOf(
-        "Albedo",
-        "Aloy",
-        "Amber",
-        "Arataki Itto",
-        "Barbara",
-        "Beidou",
-        "Bennett",
-        "Chongyun",
-        "Diluc",
-        "Diona",
-        "Eula",
-        "Fischl",
-        "Ganyu",
-        "Gorou",
-        "Hu Tao",
-        "Jean",
-        "Kaeya",
-        "Kazuha",
-        "Kamisato Ayaka",
-        "Keqing",
-        "Klee",
-        "Kojou Sara",
-        "Lisa",
-        "Mona",
-        "Ningguang",
-        "Noelle",
-        "Qiqi",
-        "Raiden Shogun",
-        "Razor",
-        "Sangonomiya Kokomi",
-        "Sayu",
-        "Sucrose",
-        "Tartaglia",
-        "Thoma",
-        "Venti",
-        "Xiangling",
-        "Xiao",
-        "Xingqiu",
-        "Xinyan",
-        "Yanfei",
-        "Yoimiya",
-        "Zhongli",
-    )
-
-
     override suspend fun invoke(event: SlashCommandEvent) {
         event.deferReply(event.isPrivate).await()
         val query = event.getOption("query")!!.asString
 
-        val results = mutableListOf<Pair<Int, String>>()
-
+        val results = mutableListOf<Pair<Int, GenshinCharacters>>()
         val score = FuzzyScore(Locale.ENGLISH)
-        characters.forEach { name ->
-            val scored = score.fuzzyScore(name, query) ?: 0
+
+        GenshinCharacters.values().forEach { c ->
+            val scored = score.fuzzyScore(c.fullname, query) ?: 0
             if (scored > 0) {
-                results.add(Pair(scored, name))
+                results.add(Pair(scored, c))
             }
         }
 
-        if (results.isEmpty()) {
-            // TODO: Could make a better error message here
+        val biggestScore = results.maxOfOrNull { it.first }
+        val closestResults = results.filter { it.first == biggestScore }
+
+        if (closestResults.isEmpty()) {
             event.hook.sendMessage("I tried really hard, but there's nothing similar in my database :(").await()
             return
         }
 
-        // sort just for testing
-        results.sortByDescending { el -> el.first }
-        val readableResults = results.take(10).joinToString(separator = "\n")
-        event.hook.sendMessage("Hello! Your query is: $query\n\nResults: \n$readableResults").await()
+        if (closestResults.size > 1) {
+            event.hook.sendMessage("There are multiple results:\n${closestResults.joinToString("\n")}").await()
+        } else {
+            event.hook.sendMessage(closestResults.joinToString("\n")).await()
+
+        }
     }
 }
