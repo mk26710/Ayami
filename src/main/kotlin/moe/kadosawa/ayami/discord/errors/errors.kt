54e8f07585221ee750a5062c0e3000601f93f255
httport <96192210+httport@users.noreply.github.com>
2022-01-08 22:48:02 +0400

    Made message option for BadArgument optional

--- src/main/kotlin/moe/kadosawa/ayami/discord/errors/errors.kt
+++ src/main/kotlin/moe/kadosawa/ayami/discord/errors/errors.kt
@@ -7,4 +7,4 @@ sealed class CommandError(message: String? = null, cause: Throwable? = null) : T
 
 class CommandInvokeError(message: String? = null, cause: Throwable) : CommandError(message, cause)
 class CheckFailure(message: String?) : CommandError(message)
-class BadArgument(message: String) : CommandError(message)
+class BadArgument(message: String?) : CommandError(message)
