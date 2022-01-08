ab7b6b2f509f9341b58a7feb078ea42ce6c2fbf7
httport <96192210+httport@users.noreply.github.com>
2022-01-08 21:57:11 +0400

    Genshin Impact service singleton
     - Holds Genshin Impact related data for characters

---
+++ src/main/kotlin/moe/kadosawa/ayami/genshin/GenshinService.kt
@@ -0,0 +1,20 @@
+package moe.kadosawa.ayami.genshin
+
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import moe.kadosawa.ayami.genshin.models.GenshinCharacter
+import java.net.URL
+
+object GenshinService {
+    private val resource: URL? = this::class.java.getResource("/characters.json")
+
+    private val json = Json {
+        prettyPrint = true
+        encodeDefaults = true
+    }
+
+    /**
+     * List of characters from resources/characters.json
+     */
+    val characters = json.decodeFromString<List<GenshinCharacter>>(resource!!.readText())
+}
