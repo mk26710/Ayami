07fc550aca498ce387a977ae93249b68144d47c2
httport <96192210+httport@users.noreply.github.com>
2022-01-08 23:23:47 +0400

    Inherit from project's base error instead of Throwable

--- src/main/kotlin/moe/kadosawa/ayami/discord/errors/errors.kt
+++ src/main/kotlin/moe/kadosawa/ayami/discord/errors/errors.kt
@@ -1,9 +1,11 @@
 package moe.kadosawa.ayami.discord.errors
 
+import moe.kadosawa.ayami.errors.AyamiError
+
 /**
  * Base error for commands
  */
-sealed class CommandError(message: String? = null, cause: Throwable? = null) : Throwable(message, cause)
+sealed class CommandError(message: String? = null, cause: Throwable? = null) : AyamiError(message, cause)
 
 class CommandInvokeError(message: String? = null, cause: Throwable) : CommandError(message, cause)
 class CheckFailure(message: String?) : CommandError(message)
