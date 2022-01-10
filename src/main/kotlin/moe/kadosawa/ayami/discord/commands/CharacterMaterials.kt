19b05c61d87bfb0b6ff90558f6670f950337e0f6
httport <96192210+httport@users.noreply.github.com>
2022-01-10 18:23:25 +0400

    Character materials command now uses recently created fuzzy search method

--- src/main/kotlin/moe/kadosawa/ayami/discord/commands/CharacterMaterials.kt
+++ src/main/kotlin/moe/kadosawa/ayami/discord/commands/CharacterMaterials.kt
@@ -4,10 +4,8 @@ import moe.kadosawa.ayami.discord.enums.MaterialType
 import moe.kadosawa.ayami.extensions.await
 import moe.kadosawa.ayami.extensions.isPrivate
 import moe.kadosawa.ayami.genshin.GenshinService
-import moe.kadosawa.ayami.genshin.models.GenshinCharacter
+import moe.kadosawa.ayami.genshin.enums.GenshinCharacters
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent
-import org.apache.commons.text.similarity.FuzzyScore
-import java.util.*
 
 object CharacterMaterials : BaseSlash() {
     override val path = "character/materials"
@@ -17,35 +15,22 @@ object CharacterMaterials : BaseSlash() {
         val query = event.getOption("query")!!.asString
         val type = MaterialType.valueOf(event.getOption("type")!!.asString.uppercase())
 
-        val results = mutableListOf<Pair<Int, GenshinCharacter>>()
-        val score = FuzzyScore(Locale.ENGLISH)
+        val bestResultEnum = GenshinCharacters.approximateByFullName(query)
 
-        GenshinService.characters.forEach { c->
-            val scored = score.fuzzyScore(c.enum.fullName, query) ?: 0
-            if (scored > 0) {
-                results.add(Pair(scored, c))
-            }
-        }
-
-        val biggestScore = results.maxOfOrNull { it.first }
-        val closestResults = results.filter { it.first == biggestScore }
-
-        if (closestResults.isEmpty()) {
-            event.hook.sendMessage("I tried really hard, but there's nothing similar in my database :(").await()
+        if (bestResultEnum == null) {
+            event.hook.sendMessage("I tried really hard but nothing was found by that query :(")
             return
         }
 
-        if (closestResults.size > 1) {
-            event.hook.sendMessage("There are multiple results:\n${closestResults.joinToString("\n")}").await()
-        } else {
-            val (_, character) = closestResults.first()
-            val msg = when (type) {
-                MaterialType.ASCENSION -> character.ascension
-                MaterialType.TALENTS -> character.talents
-            }
+        val bestResult = GenshinService.characters.find { it.enum == bestResultEnum }
 
-            event.hook.sendMessage("$msg").await()
-
+        val msg = when(type) {
+            MaterialType.ASCENSION -> bestResult?.ascension
+            MaterialType.TALENTS -> bestResult?.talents
         }
+
+        event.hook.sendMessage("$msg").await()
+
+
     }
 }
