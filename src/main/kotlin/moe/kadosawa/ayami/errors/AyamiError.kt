15552419973f7b26abd7b92c217315cf603c0c5c
httport <96192210+httport@users.noreply.github.com>
2022-01-15 22:40:50 +0400

    AyamiError was really not necessarily

--- src/main/kotlin/moe/kadosawa/ayami/errors/AyamiError.kt
+++
@@ -1,6 +0,0 @@
-package moe.kadosawa.ayami.errors
-
-/**
- * Base error for every custom error in this project
- */
-open class AyamiError(message: String? = null, cause: Throwable? = null) : Throwable(message, cause)
