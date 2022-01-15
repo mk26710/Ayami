ceb4e527f884fff5cffa05460366b7f8f09001f9
httport <96192210+httport@users.noreply.github.com>
2022-01-16 00:17:45 +0400

    Refactored DatabaseFactory
     - Renamed readyDeferred to ready
     - Updated use of Config
     - Added new createTables() method

--- src/main/kotlin/moe/kadosawa/ayami/database/DatabaseFactory.kt
+++ src/main/kotlin/moe/kadosawa/ayami/database/DatabaseFactory.kt
@@ -3,8 +3,12 @@ package moe.kadosawa.ayami.database
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
 import kotlinx.coroutines.CompletableDeferred
+import moe.kadosawa.ayami.Config
+import moe.kadosawa.ayami.database.tables.Reminders
 import mu.KotlinLogging
 import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.exists
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 import kotlin.properties.Delegates
 
@@ -12,11 +16,11 @@ private val logger = KotlinLogging.logger {}
 
 object DatabaseFactory {
     var database by Delegates.notNull<Database>()
-    val readyDeferred = CompletableDeferred<Unit>()
+    val ready = CompletableDeferred<Unit>()
 
     val hikariConfig = HikariConfig().apply {
         jdbcUrl = Config.Hikari.jdbcUrl()
-        username = Config.Hikari.user
+        username = Config.Hikari.username
         password = Config.Hikari.password
         maximumPoolSize = 10
     }
@@ -28,8 +32,19 @@ object DatabaseFactory {
 
         newSuspendedTransaction {
             exec("SELECT 1;")
-            readyDeferred.complete(Unit)
+            ready.complete(Unit)
             logger.info { "Database connection established" }
+        }
+    }
+
+    suspend fun createTables() {
+        ready.await()
+
+        newSuspendedTransaction {
+            if (!Reminders.exists()) {
+                SchemaUtils.create(Reminders)
+                logger.info { "Tables were created" }
+            }
         }
     }
 }
