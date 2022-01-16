87e7114320637b1c9c38bfc3cf340d81c6bca2e1
httport <96192210+httport@users.noreply.github.com>
2022-01-16 19:35:42 +0400

    Base class for commands

---
+++ src/main/kotlin/moe/kadosawa/ayami/commands/SlashCommand.kt
@@ -0,0 +1,21 @@
+package moe.kadosawa.ayami.commands
+
+import moe.kadosawa.ayami.CheckFailure
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent
+
+abstract class SlashCommand {
+    abstract val path: String
+
+    open suspend fun check(e: SlashCommandEvent): Boolean = true
+
+    abstract suspend fun invoke(e: SlashCommandEvent)
+
+    suspend fun run(e: SlashCommandEvent) {
+        val canRun = check(e)
+        if (!canRun) {
+            throw CheckFailure("You don't have access to this command, sorry.")
+        }
+
+        invoke(e)
+    }
+}
