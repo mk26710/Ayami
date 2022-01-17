0de69bccf979627f4c75bb7b9e1a11d7c24845fe
httport <96192210+httport@users.noreply.github.com>
2022-01-17 21:22:49 +0400

    Resin command implementation

---
+++ src/main/kotlin/moe/kadosawa/ayami/commands/ResinCommand.kt
@@ -0,0 +1,24 @@
+package moe.kadosawa.ayami.commands
+
+import moe.kadosawa.ayami.genshin.GenshinUtils
+import moe.kadosawa.ayami.jda.await
+import moe.kadosawa.ayami.jda.isPrivate
+import moe.kadosawa.ayami.jda.tryDefer
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent
+import net.dv8tion.jda.api.utils.TimeFormat
+
+class ResinCommand : SlashCommand() {
+    override val path = "resin"
+
+    override suspend fun invoke(e: SlashCommandEvent) {
+        e.tryDefer(e.isPrivate)?.await()
+
+        val current = e.getOption("current")!!.asLong
+        val needed = e.getOption("needed")!!.asLong
+
+        val replenishAt = GenshinUtils.predictResin(current, needed)
+        val discordTime = TimeFormat.DATE_TIME_LONG.format(replenishAt.toEpochMilliseconds())
+
+        e.hook.sendMessage("\uD83D\uDC4B You'll have $needed resin on $discordTime").await()
+    }
+}
