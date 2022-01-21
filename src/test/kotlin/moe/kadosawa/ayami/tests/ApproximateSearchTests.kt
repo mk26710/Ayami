63ccb41c572d77c34f4b6abdf3412c11af7b1d79
httport <96192210+httport@users.noreply.github.com>
2022-01-21 20:05:40 +0400

    Testing is love. Testing is life.

---
+++ src/test/kotlin/moe/kadosawa/ayami/tests/ApproximateSearchTests.kt
@@ -0,0 +1,45 @@
+package moe.kadosawa.ayami.tests
+
+import moe.kadosawa.ayami.genshin.Characters
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ApproximateSearchTests {
+    private fun runQueries(target: Characters, queries: List<String>) {
+        queries.forEach { query ->
+            val enum = Characters.fromSimilarName(query)
+            assertEquals(target, enum, "Querying by $query should've resulted with a different enum!")
+        }
+    }
+
+    @Test
+    fun testKamisatoAyakaLookups() {
+        val target = Characters.KAMISATO_AYAKA
+        val queries = listOf(
+            "Kamisato Ayaka",
+            "Ayaka Kamisato",
+            "Ayaka",
+            "Ayak Kasmiato",
+            "kamisato ayaya",
+            "ayaka kamisat",
+            "kmisato ayaks",
+        )
+
+        runQueries(target, queries)
+    }
+
+    @Test
+    fun testRaidenShogunLookups() {
+        val target = Characters.RAIDEN_SHOGUN
+        val queries = listOf(
+            "Raiden Shogun",
+            "Shogun Raiden",
+            "Raiden",
+            "Shogun",
+            "Baal",
+            "Ei",
+        )
+
+        runQueries(target, queries)
+    }
+}
