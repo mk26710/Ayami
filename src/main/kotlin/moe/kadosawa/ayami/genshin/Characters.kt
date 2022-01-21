1cfbb4c6b6c1c0070dcef450ce1ba562090efade
httport <96192210+httport@users.noreply.github.com>
2022-01-21 20:04:02 +0400

    Updated characters approximated lookups
     - now uses JaroWinklerSimilarity
     - improved flexibility

--- src/main/kotlin/moe/kadosawa/ayami/genshin/Characters.kt
+++ src/main/kotlin/moe/kadosawa/ayami/genshin/Characters.kt
@@ -1,81 +1,100 @@
+/*
+ * I know that this file is the ugliest of all, but
+ * my brain is too smooth for something smarter :(
+ */
+
 package moe.kadosawa.ayami.genshin
 
-import org.apache.commons.text.similarity.FuzzyScore
-import java.util.*
+import com.google.common.collect.Collections2
+import org.apache.commons.text.similarity.JaroWinklerSimilarity
+
+private fun String.permute() =
+    Collections2.permutations(split(Regex("\\s+"))).map { it.joinToString(" ") }
 
 /**
  * Enumeration of playable Genshin Impact
  * characters as of version 2.4
  */
-enum class Characters(vararg val fullNames: String) {
-    ALBEDO("Albedo"),
-    ALOY("Aloy"),
-    ARATAKI_ITTO("Arataki Itto", "Itto Arataki"),
-    BARBARA("Barbara"),
-    BEIDOU("Beidou"),
-    BENNETT("Bennett"),
-    CHONGYUN("Chongyun"),
-    DILUC("Diluc"),
-    DIONA("Diona"),
-    EULA("Eula"),
-    FISCHL("Fischl"),
-    GANYU("Ganyu"),
-    GOROU("Gorou"),
-    HU_TAO("Hu Tao"),
-    JEAN("Jean Gunnhildr", "Gunnhildr Jean"),
-    KAEDEHARA_KAZUHA("Kaedehara Kazuha", "Kazuha Kaedehara"),
-    KAEYA("Kaeya"),
-    KAMISATO_AYAKA("Kamisato Ayaka", "Ayaka Kamisato"),
-    KEQING("Keqing"),
-    KLEE("Klee"),
-    KUJOU_SARA("Kujou Sara", "Sara Kujou"),
-    LISA("Lisa"),
-    MONA("Mona"),
-    NINGGUANG("Ningguang"),
-    NOELLE("Noelle"),
-    QIQI("Qiqi"),
-    RAIDEN_SHOGUN("Ei", "Baal", "Raiden Shogun", "Shogun Raiden"),
-    RAZOR("Razor"),
-    ROSARIA("Rosaria"),
-    SANGONOMIYA_KOKOMI("Sangonomiya Kokomi", "Kokomi Sangonomiya"),
-    SAYU("Sayu"),
-    SHENHE("Shenhe"),
-    SUCROSE("Sucrose"),
-    TARTAGLIA("Tartaglia"),
-    THOMA("Thoma"),
-    VENTI("Venti"),
-    XIANGLING("Xiangling"),
-    XIAO("Xiao"),
-    XINGQIU("Xingqiu"),
-    XINYAN("Xinyan"),
-    YANFEI("Yanfei"),
-    YOIMIYA("Yoimiya"),
-    YUN_JIN("Yun Jin"),
-    ZHONGLI("Zhongli");
+enum class Characters(val aliases: List<String>) {
+    ALBEDO,
+    ALOY,
+    ARATAKI_ITTO,
+    BARBARA,
+    BEIDOU,
+    BENNETT,
+    CHONGYUN,
+    DILUC,
+    DIONA,
+    EULA,
+    FISCHL,
+    GANYU,
+    GOROU,
+    HU_TAO {
+        override val permuteNames = false
+    },
+    JEAN("Jean Gunnhildr".permute()),
+    KAEDEHARA_KAZUHA,
+    KAEYA,
+    KAMISATO_AYAKA,
+    KEQING,
+    KLEE,
+    KUJOU_SARA,
+    LISA,
+    MONA,
+    NINGGUANG,
+    NOELLE,
+    QIQI,
+    RAIDEN_SHOGUN("Ei", "Baal"),
+    RAZOR,
+    ROSARIA,
+    SANGONOMIYA_KOKOMI,
+    SAYU,
+    SHENHE,
+    SUCROSE,
+    TARTAGLIA,
+    THOMA,
+    VENTI,
+    XIANGLING,
+    XIAO,
+    XINGQIU,
+    XINYAN,
+    YANFEI,
+    YOIMIYA,
+    YUN_JIN,
+    ZHONGLI;
+
+    constructor(vararg aliases: String) : this(aliases.toList())
+
+    open val permuteNames: Boolean = true
 
     companion object {
-        fun fromFullName(s: String) =
-            values().find { c -> c.fullNames.any { name -> name == s } }
+        private fun String.normal() = trim()
+            .replace("_", " ")
+            .split("\\s+".toRegex())
+            .joinToString(" ")
+            .lowercase()
 
-        fun approximateByFullName(query: String): Characters? {
-            val results = mutableListOf<Pair<Int, Characters>>()
-            val sc = FuzzyScore(Locale.ENGLISH)
+        fun fromSimilarName(query: String): Characters? {
+            val similarity = JaroWinklerSimilarity()
+            val normalQuery = query.normal()
 
-            values().forEach { c ->
-                // Shortcut if query exactly matches any name of the character of current iteration
-                if (c.fullNames.any { it.lowercase() == query.lowercase()}) {
-                    return c
+            val results = mutableListOf<Pair<Characters, Double>>()
+
+            values().forEach { char ->
+                val aliases = char.aliases.map { it.normal() }
+                val names = if (char.permuteNames) {
+                    char.name.normal().permute()
+                } else {
+                    listOf(char.name.normal())
                 }
 
-                c.fullNames.forEach { name ->
-                    val scored = sc.fuzzyScore(query, name)
-                    if (scored > 1) {
-                        results.add(Pair(scored, c))
-                    }
+                (names + aliases).forEach { name ->
+                    val score = similarity.apply(normalQuery, name)
+                    results.add(Pair(char, score))
                 }
             }
 
-            return results.maxWithOrNull(compareBy { it.first })?.second
+            return results.maxWithOrNull(compareBy { it.second })?.first
         }
     }
 }
