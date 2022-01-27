eddf125091f6d7618be09ebf243c112f3b642603
httport <96192210+httport@users.noreply.github.com>
2022-01-27 22:56:11 +0400

    Brought back commands refresh command

---
+++ src/main/kotlin/moe/kadosawa/ayami/commands/RefreshCommands.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2022 kadosawa (kadosawa.moe)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package moe.kadosawa.ayami.commands
+
+import moe.kadosawa.ayami.Ayami
+import moe.kadosawa.ayami.BadArgument
+import moe.kadosawa.ayami.abc.SlashCommand
+import moe.kadosawa.ayami.jda.await
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent
+
+class RefreshCommands : SlashCommand("refresh-commands") {
+    override suspend fun check(e: SlashCommandEvent) =
+        e.user.id == Ayami.appInfo.owner.id
+
+    override suspend fun invoke(e: SlashCommandEvent) {
+        e.deferReply().await()
+
+        val global = e.getOption("global")!!.asBoolean
+        val debug = e.getOption("debug")!!.asBoolean
+
+        if (!global && !debug) {
+            throw BadArgument("What do you expect? Both arguments are false, nothing was refreshed.")
+        }
+
+        Ayami.refreshCommands(global, debug)
+        e.hook.sendMessage("Commands update request was sent").await()
+    }
+}
